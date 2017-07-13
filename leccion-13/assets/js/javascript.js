3f74791a6eee23579287a0958a126042393a8872
aydee <aydee.qm@gmail.com>
2017-07-13 17:14:49 -0500

    codificando js y html

---
+++ leccion-13/assets/js/javascript.js
@@ -0,0 +1,38 @@
+var frutas = [];
+var records = document.getElementById('records');
+
+function master(fruta) {
+  this.fruta = fruta;
+};
+
+master.prototype.toHTML = function () {
+  var html = '';
+  html += ':) '+ this.fruta + '<br>';
+  html += '<br><br>';
+  return html;
+}
+
+function mergeHTML (){
+  var html = '';
+  for (var i=0 ;i < frutas.length; i++){
+    html += frutas[i].toHTML();
+  }
+  return html;
+}
+
+function printHTML (html){
+  records.innerHTML = '';
+  records.innerHTML = html;
+}
+var addFruta = document.getElementById('nuevacompra');
+addFruta.onclick = function() {
+  var agregar = document.getElementById('fruit').value;
+  var product  = new master(fruta);
+  frutas.push(product);
+  printHTML(product.toHTML());
+};
+
+var printAll = document.getElementById('print');
+printAll.onclick = function() {
+  printHTML(mergeHTML());
+}
